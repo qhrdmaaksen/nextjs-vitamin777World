c44da3cb66aa5e75cf6bdb8b4bbe2314296bc929
vitamin7777777 <rlaalsdn8@gmail.com>
2024-10-13 19:04:44 +0900

    feet: 자동 재배포를 위한 github action file 생성 및 설정
    - 주간 재배포를 위한 설정 및 VERCEL 에서 token, org_id, project_id 를 github project settings 에서 action 추가

--- components/banner/LeftSideBanner.js
+++ components/banner/LeftSideBanner.js
@@ -6,20 +6,24 @@ import {
 } from '../../atoms/stateAtoms';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
-// Props 로 들어올 BannerImages 는 이미지의 경로를 담은 배열
+// LeftSideBanner 컴포넌트는 슬라이드 배너를 구현합니다.
 // interval 은 이미지가 전환되는 시간 간격을 나타냄
-
 const LeftSideBanner = ({ interval }) => {
   {
     // currentSlideImage 는 현재 보여지는 이미지의 index 를 나타냄
     const [currentSlideImage, setCurrentSlideImage] = useRecoilState(
+        // Recoil 상태에서 현재 슬라이드 이미지 인덱스를 가져오고 설정
       leftCurrentSlideImageState,
     );
+
+    // Recoil 상태에서 가져온 슬라이드 이미지 경로 배열
     const LeftBannerImages = useRecoilValue(leftSlideImageState);
 
     // useEffect 를 이용하여 interval 마다 currentSlideImage 를 변경
     useEffect(() => {
+      // setInterval 을 사용해 주어진 interval 마다 currentSlideImage 를 업데이트
       const leftNextSlideTimer = setInterval(() => {
+        // 이전 이미지 인덱스에 1을 더한 후 이미지 배열의 길이로 나눈 나머지 값을 사용하여 이미지를 순환하도록 설정
         setCurrentSlideImage(
           (prevImage) => (prevImage + 1) % LeftBannerImages.length,
         );
